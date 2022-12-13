510eed59d1586d5c6ed741abf147f135346509eb
Pydasaikrishna <saikrishna.pyda9951@gmail.com>
2022-12-13 21:45:35 +0530

    Hackerrank probelm-1

---
+++ problem-1.py
@@ -0,0 +1,30 @@
+# Sai = int(input("Enter the Number: "))
+# if (Sai%2==1):
+#     print("Weird")
+# elif (Sai%2==0 and 2<=Sai<=5):
+#     print("Not Weird")
+# elif (Sai%2==0 and 6<=Sai<=20):
+#     print("Weird1")
+# else:
+#     print("Not Weird1")
+
+
+# a = int(input("Enter the Number-1: "))
+# b = int(input("Enter the Number-2: "))
+# print("Sum of two numbers: ",a+b)
+# # print("Difference between two numbers: ",a-b)
+# if (a-b < 0):
+#     print("Enter other number!!!!")
+# else:
+#     print("Difference between two numbers are: ",a-b)
+# print("Product of two numbers: ",a*b)
+
+
+
+i = int(input("Enter the value: "))
+for i in range(0,i+1):
+    print(i)
+# else:
+#     print("In between Numbers")
+
+
